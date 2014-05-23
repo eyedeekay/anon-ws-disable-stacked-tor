b188c078f971faf44bc5ff47cb75126b509ab6ac
Patrick Schleizer <adrelanos@riseup.net>
2014-05-23 17:46:46 +0000

    .

---
+++ etc/profile.d/20_controlportfilt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+## This file is part of Whonix.
+## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+## See the file COPYING for copying conditions.
+
+if [ -x /usr/lib/anon-ws-disable-stacked-tor/controlportfilt.sh ]; then
+   . /usr/lib/anon-ws-disable-stacked-tor/controlportfilt.sh
+fi
