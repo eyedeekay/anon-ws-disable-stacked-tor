6093a880105191722b0c0707078b70bb486fadf3
Patrick Schleizer <adrelanos@riseup.net>
2016-06-15 21:46:30 +0000

    nullglob

--- usr/lib/anon-ws-disable-stacked-tor/controlportfilt.sh
+++ usr/lib/anon-ws-disable-stacked-tor/controlportfilt.sh
@@ -6,33 +6,33 @@
 
 ## {{{ controlportfilt.d
 
-if [ -d /etc/controlportfilt.d ]; then
-   for i in /etc/controlportfilt.d/* /rw/config/controlportfilt.d/*.conf; do
-      if [ -f "$i" ]; then
-         ## If the last character is a ~, ignore that file,
-         ## because it was created by some editor,
-         ## which creates backup files.
-         if [ "${i: -1}" = "~" ]; then
-            continue
-         fi
-         ## Skipping files such as .dpkg-old and .dpkg-dist.
-         if ( echo "$i" | grep -q ".dpkg-" ); then
-            true "skip $i"
-            continue
-         fi
-         bash_n_exit_code="0"
-         bash_n_output="$(bash -n "$i" 2>&1)" || { bash_n_exit_code="$?" ; true; };
-         if [ ! "$bash_n_exit_code" = "0" ]; then
-            echo "Invalid config file: $i
+shopt -s nullglob
+for i in /etc/controlportfilt.d/* /rw/config/controlportfilt.d/*.conf; do
+   if [ -f "$i" ]; then
+      ## If the last character is a ~, ignore that file,
+      ## because it was created by some editor,
+      ## which creates backup files.
+      if [ "${i: -1}" = "~" ]; then
+         continue
+      fi
+      ## Skipping files such as .dpkg-old and .dpkg-dist.
+      if ( echo "$i" | grep -q ".dpkg-" ); then
+         true "skip $i"
+         continue
+      fi
+      bash_n_exit_code="0"
+      bash_n_output="$(bash -n "$i" 2>&1)" || { bash_n_exit_code="$?" ; true; };
+      if [ ! "$bash_n_exit_code" = "0" ]; then
+         echo "Invalid config file: $i
 bash_n_exit_code: $bash_n_exit_code
 bash_n_output:
 $bash_n_output" >&2
-         else
-            source "$i"
-         fi
+      else
+         source "$i"
       fi
-   done
-fi
+   fi
+done
+
 
 ## }}}
 
