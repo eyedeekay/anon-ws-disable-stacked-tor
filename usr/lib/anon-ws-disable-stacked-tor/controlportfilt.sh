28e0d4ce831732bf042e7ba73cf650e3462ae81b
Patrick Schleizer <adrelanos@riseup.net>
2016-06-15 21:50:24 +0000

    code simplification

--- usr/lib/anon-ws-disable-stacked-tor/controlportfilt.sh
+++ usr/lib/anon-ws-disable-stacked-tor/controlportfilt.sh
@@ -8,31 +8,17 @@
 
 shopt -s nullglob
 for i in /etc/controlportfilt.d/*.conf /rw/config/controlportfilt.d/*.conf; do
-   if [ -f "$i" ]; then
-      ## If the last character is a ~, ignore that file,
-      ## because it was created by some editor,
-      ## which creates backup files.
-      if [ "${i: -1}" = "~" ]; then
-         continue
-      fi
-      ## Skipping files such as .dpkg-old and .dpkg-dist.
-      if ( echo "$i" | grep -q ".dpkg-" ); then
-         true "skip $i"
-         continue
-      fi
-      bash_n_exit_code="0"
-      bash_n_output="$(bash -n "$i" 2>&1)" || { bash_n_exit_code="$?" ; true; };
-      if [ ! "$bash_n_exit_code" = "0" ]; then
-         echo "Invalid config file: $i
+   bash_n_exit_code="0"
+   bash_n_output="$(bash -n "$i" 2>&1)" || { bash_n_exit_code="$?" ; true; };
+   if [ ! "$bash_n_exit_code" = "0" ]; then
+      echo "Invalid config file: $i
 bash_n_exit_code: $bash_n_exit_code
 bash_n_output:
 $bash_n_output" >&2
-      else
-         source "$i"
-      fi
+      exit 1
    fi
+   source "$i"
 done
-
 
 ## }}}
 
