f16e1fea8b0f265cc138ad93fb3b39ac7a678c15
Patrick Schleizer <adrelanos@riseup.net>
2017-06-06 02:07:37 +0200

    comments

--- usr/lib/anon-ws-disable-stacked-tor/torbrowser.sh
+++ usr/lib/anon-ws-disable-stacked-tor/torbrowser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## This file is part of Whonix.
-## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+## Copyright (C) 2012 - 2017 Patrick Schleizer <adrelanos@riseup.net>
 ## See the file COPYING for copying conditions.
 
 ## Deactivate tor-launcher,
@@ -16,22 +16,31 @@ export TOR_SKIP_LAUNCH=1
 ##    "TOR_SOCKS_HOST, TOR_SOCKS_PORT regression"
 ##    https://trac.torproject.org/projects/tor/ticket/8336
 ## (As an alternative,
-##    /home/user/tor-browser_en-US/Data/profile/user.js
+##    /home/user/tor-browser/Browser/TorBrowser/Data/Browser/profile.default/user.js
 ## could be used.)
 ## Fortunately, this is not required for Whonix by default anymore,
-## because since Whonix 0.6.2, socat is configured to redirect
+## because since Whonix 13, socat is configured to redirect
 ## Whonix-Workstation ports
-##   127.0.0.1:9050 to Whonix-Gateway 10.152.152.10:9050 and
-##   127.0.0.1:9150 to Whonix-Gateway 10.152.152.10:9150.
+##   127.0.0.1:9050 to Whonix-Gateway 10.152.152.10:9050
+##   127.0.0.1:9051 to Whonix-Gateway 10.152.152.10:9051
+##   127.0.0.1:9150 to Whonix-Gateway 10.152.152.10:9150
+##   127.0.0.1:9151 to Whonix-Gateway 10.152.152.10:9151
 #export TOR_SOCKS_HOST="10.152.152.10"
 #export TOR_SOCKS_PORT="9150"
+#export TOR_CONTROL_HOST="127.0.0.1"
+#export TOR_CONTROL_PORT="9151"
+## this is to satisfy Tor Button just filled up with anything
+#export TOR_CONTROL_PASSWD='"password"'
 
+## We are not using TOR_TRANSPROXY=1 because that would break Tor Browser's
+## per tab stream isolation. (Tor Browser talks to a Tor SocksPort and sets a
+## socks user name and Tor is using IsolateSOCKSAuth by Tor default.)
 #export TOR_TRANSPROXY=1
 
 ## Environment variable to configure Tor Browser to use a pre existing unix
 ## domain socket file instead of creating its own one to avoid Tor over Tor and
 ## to keep it being able to connect.
-## Since Whonix 12, socat is creating a unix domain socket file
+## Since Whonix 13, socat is creating a unix domain socket file
 ## /var/run/anon-ws-disable-stacked-tor/127.0.0.1_9150.sock which forwards to
 ## 127.0.0.1:9150, where socat is listening, which forwards it to the gateway
 ## port 9150.
